a161e7520dc840e98cab95ca446fcc792d0d6149
Ray <Zhuoxun.Yang777@outlook.com>
2023-03-11 10:36:21 +0800

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -6,13 +6,27 @@ int
 main(int argc, char *argv[])
 {
 	int pid;
+	char* message = "message";
+	int messagesize = 8;
+	int p[2];
+
+	if(pipe(p) < 0){
+		fprintf(2, "pipe failed\n");
+		exit(1);
+	}
 
 	pid = fork();
 
 	if(pid == 0){
+		read(p[1], message, messagesize);
+		write(p[0], message, messagesize);
+
 		fprintf(1, "%d: received ping\n", getpid());
 	} else{
-		file = pipe(
+		write(p[1], message, messagesize);
+		wait(0);
+		read(p[0], message, messagesize);
+
 		fprintf(1, "%d: received pong\n", getpid());
 	}
 
